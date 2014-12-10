647c0b6884406ca3e701e1cb85f61c122b0dc37e
Marc Bolaños Solà <marc.bolanos@ub.edu>
2014-12-10 17:53:47 +0100

    Added Main and TestCVLibrary Classes.

---
+++ src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import Test.TestCBLibrary;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Create Case Base Library test class
+		TestCBLibrary testcbl = new TestCBLibrary();
+		
+	}
+
+}
